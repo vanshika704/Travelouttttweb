c7c005f1c85aeadb536c802dffadccaa5b1d3b19
vanshika704 <sharmavanshi704@gmail.com>
2024-09-09 15:34:12 +0530

    //

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,16 +1,16 @@
 function Images() {
   return (
     <>
-      <div className="h-screen bg-zinc-800 pt-7">
+      <div className="h-[200vh] bg-zinc-800 pt-7">
         <div className="text-white font-poppins text-5xl mb-10 ml-16">Top Destinations</div>
         <div className="flex justify-center">
-          <div className="grid grid-cols-3 gap-6 px-16">
+          <div className="grid grid-cols-4 gap-6 px-16">
             {/* Card 1 */}
-            <div className="flex flex-col gap-1">
-              <a href="#" className="bg-lime-200">
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200 w-60" >
                 <img
                   src="src/assets/image1.jpg"
-                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-80"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
                   alt="Destination 1"
                 />
               </a>
@@ -23,11 +23,11 @@ function Images() {
             </div>
 
             {/* Card 2 */}
-            <div className="flex flex-col gap-1">
-              <a href="#" className="bg-lime-200">
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200 w-60">
                 <img
                   src="src/assets/image2.jpg"
-                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-80"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
                   alt="Destination 2"
                 />
               </a>
@@ -40,15 +40,45 @@ function Images() {
             </div>
 
             {/* Card 3 */}
-            <div className="flex flex-col gap-1">
-              <a href="#" className="bg-lime-200">
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200 w-60">
                 <img
                   src="src/assets/image3.jpg"
-                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-80"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
                   alt="Destination 3"
                 />
               </a>
               <a href="#" className="hover:text-lime-200 text-gray-200 font-semibold">MALDIVES</a>
+              <a href="#" className="hover:text-purple-500 text-sm text-gray-400 -mt-1">43.8K successful visits</a>
+              <div className="flex flex-row flex-wrap gap-2">
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Book</a>
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Budget</a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200">
+                <img
+                  src="src/assets/pexels-habi-dompil-1539296-2964163.jpg"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
+                  alt="Destination 2"
+                />
+              </a>
+              <a href="#" className="hover:text-lime-200 text-gray-200 font-semibold"> VIETNAM</a>
+              <a href="#" className="hover:text-purple-500 text-sm text-gray-400 -mt-1">56.3K successful visits</a>
+              <div className="flex flex-row flex-wrap gap-2">
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Book</a>
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Budget</a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200 w-60">
+                <img
+                  src="src/assets/pexels-tracehudson-2516418.jpg"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
+                  alt="Destination 3"
+                />
+              </a>
+              <a href="#" className="hover:text-lime-200 text-gray-200 font-semibold">TIBET</a>
               <a href="#" className="hover:text-purple-500 text-sm text-gray-400 -mt-1">43.8K successful visits</a>
               <div className="flex flex-row flex-wrap gap-2">
                 <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Book</a>
