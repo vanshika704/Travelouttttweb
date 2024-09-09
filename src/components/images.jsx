7137ce7015ccf2f2431707ec4614988530eda07e
vanshika704 <sharmavanshi704@gmail.com>
2024-09-09 15:35:25 +0530

    hello

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -85,6 +85,21 @@ function Images() {
                 <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Budget</a>
               </div>
             </div>
+            <div className="flex flex-col gap-2">
+              <a href="#" className="bg-lime-200">
+                <img
+                  src="src/assets/pexels-habi-dompil-1539296-2964163.jpg"
+                  className="hover:translate-x-1 hover:-translate-y-1 transition-transform duration-100 h-80 w-60"
+                  alt="Destination 2"
+                />
+              </a>
+              <a href="#" className="hover:text-lime-200 text-gray-200 font-semibold"> VIETNAM</a>
+              <a href="#" className="hover:text-purple-500 text-sm text-gray-400 -mt-1">56.3K successful visits</a>
+              <div className="flex flex-row flex-wrap gap-2">
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Book</a>
+                <a href="#" className="hover:bg-gray-600 text-gray-300 text-xs font-semibold bg-gray-700 px-2 py-1 rounded-full">Budget</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
